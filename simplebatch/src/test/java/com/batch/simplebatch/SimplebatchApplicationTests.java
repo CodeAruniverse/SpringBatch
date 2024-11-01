154cf022ecc200d62c49027d8a07df902e1fddac
CodeAruninverse <kumarun009@gmail.com>
2024-11-01 18:37:05 +0530

    batc demo project

---
+++ simplebatch/src/test/java/com/batch/simplebatch/SimplebatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.simplebatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplebatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
